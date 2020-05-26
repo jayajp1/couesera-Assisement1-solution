4bba9419dde580c17ee6f849525273afef3d24e9
Jay Limbachiya <jaylimbachiya123456@gmail.com>
2020-05-26 20:24:41 +0530

    my second Assignment

--- App.js
+++ App.js
@@ -1,37 +1,114 @@
+(function () {
+  'use strict';
+  
+  angular.module('ShoppingListApp', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListService', ShoppingListService)
+  .provider('ShoppingListService',ShoppingListServiceProvider).config(Config);
 
-(function(){
-'Use strict'
-   angular.module('MyApp',[]).
-        controller('MyCtrl', function($scope)
-            { 
-                $scope.names="";
-                $scope.countNames = function () { 
-               
-                    if($scope.names.length == "")   
-                    {
-                        $scope.message="enter something";
-                    } 
-                    else{
-                        if($scope.names.length==1)
-                        {
-                            $scope.message="enjoy!" ;
-                        }
-                        else{
-                            var totalNames=$scope.names.split(",");
-                            var len=totalNames.length;
-                            if(len>3)
-                            {
-                               $scope.message="too much!";
-                            }
-                            if( len>1 && len<4){
-                                  $scope.message="enjoy!" ;
-                               }
+  Config.$inject = ['ShoppingListServiceProvider'];
+  function Config(ShoppingListServiceProvider) {
+    ShoppingListServiceProvider.defaults.minItems = 5;
+  }
+  
 
-                        }
+  ToBuyController.$inject = ['ShoppingListService'];
+  function ToBuyController(ShoppingListService) {
+    var showList = this;
+  
+    showList.items = ShoppingListService.getItems();
+    console.log(showList.items.length);
+      if(showList.items.length===0){
+        console.log(showList.items.length);
+        showList.error1="Everything is bought";
+        //showList.error2="Everything is bought";
+      }
+      else{
+        showList.removeItem = function (itemIndex) {
+          console.log("re")
+          ShoppingListService.removeItem(itemIndex);
+        };
+      }
+    }
 
-                      
-                    }
-                   
-                } 
-            });
-})();
+    AlreadyBoughtController.$inject = ['ShoppingListService'];
+    function AlreadyBoughtController(ShoppingListService) {
+    var showList = this;
+    var items;
+      if(showList.items2===undefined){
+        showList.error1="nothing Bought yet...";
+      }
+      //else if(showList.items.length==='0')
+        showList.items2= ShoppingListService.getItems2();
+    // }
+    
+      
+    
+  }
+
+  function ShoppingListService(minItems) {
+    var service = this;
+
+    // List of shopping items
+    var items2=[];
+    var items = [
+         {itemName: 'Cokkies', itemQuantity:5 },
+          {itemName: 'Coke', itemQuantity:5 },
+          {itemName: 'Cupecake', itemQuantity:5 },
+          {itemName: 'Pizza', itemQuantity:5 },
+          {itemName: 'Puff', itemQuantity:5 }
+    ];
+  
+    service.addItem = function (itemName, quantity) {
+        console.log(itemName,quantity)
+        console.log(items.length,items2.length);
+        var l=0;
+         // console.log(items.length,minItems);
+          var item = {
+            itemName: itemName,
+            itemQuantity: quantity
+          };
+          items2.push(item);
+        
+    };
+  
+    service.removeItem = function (itemIndex) {
+      var showList=this;
+     var it=items.splice(itemIndex, 1);
+     var l=0;
+     console.log(it[0].itemName,it[0].itemQuantity);
+     if(items.length===0){
+      showList.error1 = "knsni"; 
+     }
+     else{
+      try{
+        service.addItem(it[0].itemName,it[0].itemQuantity);
+       }
+       catch(error){
+        showList.error1 = error.message;
+        showList.error2 = error.message;
+       }
+     }
+    
+    };
+
+    service.getItems = function () {
+      return items;
+    };
+    service.getItems2 = function () {
+      return items2;
+    };
+  }
+  function ShoppingListServiceProvider() {
+    var provider = this;
+    provider.defaults = {
+      minItems: 0
+    };
+    provider.$get = function () {
+      var shoppingList = new ShoppingListService(provider.defaults.minItems);
+      return shoppingList;
+    };
+  }
+  })();
+  
